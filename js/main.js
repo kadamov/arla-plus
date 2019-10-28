e778121d6a438b339dc176a73df9336b2bf5a428
Konstantin <kadamov.work@gmail.com>
2019-10-29 01:46:53 +0200

    Farm table - update + View farmer profile - koko

--- js/main.js
+++ js/main.js
@@ -108,3 +108,15 @@ loader.classList.remove("hide");
 loader.classList.add("hide");
 }
 }
+
+var container = $('#container');
+
+$(document).on('click', '.list-view li', function(){
+  $(this).addClass('active');
+  container.addClass('details');
+});
+
+$('.back').click(function(){
+  container.removeClass('details');
+  $('.list-view li').removeClass('active');
+});
